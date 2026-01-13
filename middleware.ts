978f25ff62db8e7bfc013b32213a777db5fbfe6c
Yannis Moniaros <moniaros@gmail.com>
2026-01-13 17:31:07 +0200

    fix Manifest: Line: 1, column: 1, Syntax error.

--- middleware.ts
+++ middleware.ts
@@ -98,6 +98,6 @@ export const config = {
          * - favicon.ico (favicon file)
          * - public folder
          */
-        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!_next/static|_next/image|favicon.ico|manifest.json|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
